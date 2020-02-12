a33cf8b0dcc6e82d1a4349a0211124e38f261b63
Hygerta Imeri <hygerta.imeri@hotmail.com>
2020-02-13 01:27:08 +0300

    remove duplicates

--- notive_app/lib/screens/profile_screen.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ProfileScreen extends StatelessWidget {
-  static const String id = 'profile_screen';
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
