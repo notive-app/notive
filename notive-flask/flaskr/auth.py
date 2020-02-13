c5febdd456e4326e374bff69d089d42ba92577ba
Ege Hatırnaz <egehatirnaz@hotmail.com>
2020-02-13 19:19:34 +0300

    Auth functions are completely validated and reworked.

--- notive-flask/flaskr/auth.py
+++ notive-flask/flaskr/auth.py
@@ -5,9 +5,8 @@ from flask import (
     Blueprint, g, request, session, jsonify, make_response, Response
 )
 from werkzeug.security import check_password_hash, generate_password_hash
-from werkzeug.exceptions import abort
 from sqlalchemy import Table
-from .env import valid_auth
+from .util import validate_auth_key, get_json_from_keys
 
 from .db import get_db
 
@@ -16,81 +15,80 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods=['POST'])
 def register():
-    if 'Auth-Key' in request.headers:
-        auth_key = request.headers['Auth-Key']
-        if valid_auth(auth_key):
-            if request.method == 'POST':
-                if not request.is_json:  # JSON control.
-                    return make_response(jsonify(
-                        {"message": "Request body must be JSON."}), 400)
-
-                json_data = request.get_json()
-                if 'name' in json_data and 'surname' in json_data and\
-                        'password' in json_data and 'email' in json_data and 'date_of_birth' in json_data:
-                    name = json_data['name']
-                    surname = json_data['surname']
-                    password_plain = json_data['password']
-                    email = json_data['email']
-                    dob = json_data['date_of_birth']
-
-                    db = get_db()
-                    con, engine, metadata = db['con'], db['engine'], db['metadata']
-                    users = Table('User', metadata, autoload=True)
-
-                    msg = {"message": "Something went wrong."}
-                    if not name or not surname or not password_plain or not email or not dob:
-                        msg = {"message": "Error: Missing parameters!"}
-                        return make_response(jsonify(msg, 400))
-
-                    if users.select(users.c.email == email).execute().first():
-                        msg = {"message": "There is an existing user with this e-mail address!"}
-                        return make_response(jsonify(msg, 400))
-
-                    con.execute(users.insert(), name=name, surname=surname, email=email,
-                                password=generate_password_hash(password_plain), date_of_birth=dob,
-                                created_at=int(time.time()))
-                    msg = {"message": "You have registered successfully!"}
-                    return make_response(jsonify(msg, 200))
-                else:
-                    return make_response(jsonify({"message": "Invalid parameters."}), 400)
-            else:
-                return Response(status=405)
-        else:
-            return Response(status=401)
-    else:
+    if not validate_auth_key(request):
         return Response(status=401)
+    else:
+        json_data = get_json_from_keys(request, ['name', 'surname', 'password', 'email', 'date_of_birth'])
+        if json_data is False:
+            return make_response(jsonify(
+                {"message": "Request body must be JSON."}), 400)
+        elif json_data is None:
+            return make_response(jsonify({"message": "Invalid parameters."}), 400)
+        else:
+            name = json_data['name']
+            surname = json_data['surname']
+            password_plain = json_data['password']
+            email = json_data['email']
+            dob = json_data['date_of_birth']
+
+            db = get_db()
+            con, engine, metadata = db['con'], db['engine'], db['metadata']
+            users = Table('User', metadata, autoload=True)
+
+            msg = {"message": "Something went wrong."}
+            if not name or not surname or not password_plain or not email or not dob:
+                msg = {"message": "Error: Missing parameters!"}
+                return make_response(jsonify(msg, 400))
+
+            if users.select(users.c.email == email).execute().first():
+                msg = {"message": "There is an existing user with this e-mail address!"}
+                return make_response(jsonify(msg, 400))
+
+            con.execute(users.insert(), name=name, surname=surname, email=email,
+                        password=generate_password_hash(password_plain), date_of_birth=dob,
+                        created_at=int(time.time()))
+            msg = {"message": "You have registered successfully!"}
+            return make_response(jsonify(msg, 200))
 
 
 @bp.route('/login', methods=['POST'])
 def login():
-    password = request.form.get('password')
-    email = request.form.get('email')
-
-    msg = ""
-    if not email or not password:
-        msg = {"status": {"type": "failure", "message": "Missing Data"}}
-        abort(400, msg)
-
-    db = get_db()
-    con, engine, metadata = db['con'], db['engine'], db['metadata']
-    users = Table('User', metadata, autoload=True)
-    user = users.select(users.c.email == email).execute().first()
-
-    if user is None or not check_password_hash(user['password'], password):
-        msg = {"status": {"type": "failure", "message": "Username or password incorrect"}}
-        abort(400, msg)
+    if not validate_auth_key(request):
+        return Response(status=401)
     else:
-        msg = {"status": {"type": "success",
-                          "message": "You logged in"},
-               "data": {"user": {"email": user['email'],
-                                 "name": user['name'],
-                                 "surname": user['surname']}
-                        }}
+        json_data = get_json_from_keys(request, ['email', 'password'])
+        if json_data is False:
+            return make_response(jsonify(
+                {"message": "Request body must be JSON."}), 400)
+        elif json_data is None:
+            return make_response(jsonify({"message": "Invalid parameters."}), 400)
+        else:
+            email = json_data['email']
+            password = json_data['password']
 
-        session.clear()
-        session['user_id'] = user['id']
+            msg = {"message": "Something went wrong."}
+            if not email or not password:
+                msg = {"message": "Error: Missing parameters!"}
+                return make_response(jsonify(msg, 400))
 
-    return jsonify(msg)
+            db = get_db()
+            con, engine, metadata = db['con'], db['engine'], db['metadata']
+            users = Table('User', metadata, autoload=True)
+            user = users.select(users.c.email == email).execute().first()
+
+            if user is None or not check_password_hash(user['password'], password):
+                msg = {"message": "Error: Invalid username or password!"}
+                return make_response(jsonify(msg, 400))
+            else:
+                msg = {"message": "You have been logged in successfully!",
+                       "data": {"user": {"email": user['email'],
+                                         "name": user['name'],
+                                         "surname": user['surname']}
+                                }}
+                session.clear()
+                session['user_id'] = user['id']
+
+                return make_response(jsonify(msg, 200))
 
 
 @bp.before_app_request
@@ -115,8 +113,8 @@ def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            msg = {"status": {"type": "failure", "message": "Login is required!"}}
-            return jsonify(msg)
+            msg = {"message": "Error: Login is required!"}
+            return make_response(jsonify(msg, 400))
 
         return view(**kwargs)
 
