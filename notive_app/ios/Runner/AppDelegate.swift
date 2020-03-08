78e386b272d60b61e0c91d18b2f187e2cd0f2ede
Gerta <hygerta.imeri@hotmail.com>
2020-03-08 23:04:21 +0300

    kucucuk seyler

--- notive_app/ios/Runner/AppDelegate.swift
+++ notive_app/ios/Runner/AppDelegate.swift
@@ -8,6 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("KEY HEREEE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
