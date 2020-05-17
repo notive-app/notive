809408935cd0d2f53855c742ff9c68804f654d55
Ege <egehatirnaz@hotmail.com>
2020-05-17 16:54:24 +0300

    Basic tracking functionality.

--- notive_app/lib/components/map.dart
+++ notive_app/lib/components/map.dart
@@ -1,33 +1,66 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:notive_app/models/user_model.dart';
 import 'package:provider/provider.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Map extends StatelessWidget {
+  GoogleMapController _controller;
+  bool tracking = false;
+
   _onMapCreated(UserModel user) {
     user.setAllItemVenues();
   }
 
-  _onCameraMove(CameraPosition position) {}
+  _onCameraMove(CameraPosition position) {
+  }
+
+  _animateToUser() async {
+    var pos = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+    _controller.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+          target: LatLng(pos.latitude, pos.longitude),
+          zoom: 14.4746,
+        )
+      )
+    );
+
+    // TODO: RESET ALL ITEM VENUES!
+  }
+
+  toggleTracking(){
+    print("Tracking running.");
+    tracking = !tracking;
+    if(tracking){
+      _animateToUser();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Consumer<UserModel>(builder: (context, user, child) {
       return Container(
           child: GoogleMap(
-        markers: user.getMarkers(),
-        mapType: MapType.normal,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(double.parse(user.lat), double.parse(user.long)),
-          zoom: 14.4746,
-        ),
-        onMapCreated: _onMapCreated(user),
-        zoomGesturesEnabled: true,
-        onCameraMove: _onCameraMove,
-        myLocationEnabled: true,
-        compassEnabled: true,
-        myLocationButtonEnabled: false,
+            markers: user.getMarkers(),
+            mapType: MapType.normal,
+            initialCameraPosition: CameraPosition(
+              target: LatLng(double.parse(user.lat), double.parse(user.long)),
+              zoom: 14.4746,
+            ),
+            onMapCreated: (GoogleMapController controller) {
+                            _controller = controller;
+                            _onMapCreated(user);
+                          },
+            zoomGesturesEnabled: true,
+            onCameraMove: _onCameraMove,
+            myLocationEnabled: true,
+            compassEnabled: true,
+            myLocationButtonEnabled: false,
       ));
     });
   }
